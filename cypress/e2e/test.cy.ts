c9bcc53b6d38036c783e27120041600f211aeabb
Beau Emanuel <bemanuel3@gmail.com>
2024-10-16 11:48:18 -0600

    Add initial tests

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,5 +1,33 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
-  })
-})
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('the grid is created with the right size when the size is entered', () => {
+    cy.get('input').type('3');
+    cy.get('button').click();
+    cy.get('#table').should('exist');
+  });
+
+  // it('the correct grid square is selected when entered into the input', () => {
+  //   cy.get('#table').should('exist');
+
+  //   cy.get('input').type('3');
+  //   cy.get('button').click();
+  //   cy.get('#2').should
+  // });
+
+  it.only('enables players to play a game of tictactoe', () => {
+    cy.get('input').type('3');
+    cy.get('button').click();
+    cy.get('#table').should('exist');
+
+    cy.get('#4').click();
+    cy.get('#0').click();
+    cy.get('#2').click();
+    cy.get('#1').click();
+    cy.get('#6').click();
+
+    cy.get('#endgame').contains(`Congratulations player X! You've won. Refresh to play again!`, { matchCase: false });
+  });
+});
